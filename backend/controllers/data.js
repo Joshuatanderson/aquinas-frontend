2365f4af0165c05b98230738d1c312407725690d
Joshua Anderson <joshuatanderson1@gmail.com>
2021-06-20 16:25:57 -0500

    feat: so... it fricking works now.

--- backend/controllers/data.js
+++ backend/controllers/data.js
@@ -1,9 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+const dotenv = require("dotenv");
 
+dotenv.config({ path: `${__dirname}/config.env` });
 
-exports.getData = async (req, res) => {
-	console.log("well, I'm kind of here")
-	res.status(203).json({
-		status: "success",
-		data: {}
-	})
+exports.getData = async (req, res, next) => {
+	console.info("server connection live");
+
+	const uri =`mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.wcadp.mongodb.net/aquinas?retryWrites=true&w=majority`;
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+	client.connect(async (err) => {
+		try {
+			const db = await client.db("aquinas");
+			const docs = await db.collection('data').find({}).toArray();
+
+			// perform actions on the collection object
+			await res.status(203).json({
+				status: "success",
+				data: docs
+			})
+			client.close();
+		} catch(err){
+			res.status(500).json({
+				status: "error",
+				data: {err}
+			})
+		}
+	});
+
+	
 }
