9bb8c5bf6a3220a8acdf5f7ea5de666264a48a56
Joshua Anderson <joshua.anderson@bluefindata.com>
2021-09-21 12:54:38 -0500

    feat: handle chapter:verse searches

--- backend/controllers/search.js
+++ backend/controllers/search.js
@@ -3,25 +3,24 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: `${__dirname}/config.env` });
 
-const MOCKED_PARAMS = ["Genesis", "book 4"]
-
 /**
- * 
- * @param {array of strings} params 
+ *
+ * @param {array of strings} params
  * @returns string of the format "\"param1\" \"param 2\""
  */
 
 const queryifySearchTerms = (params) => {
-	const modifiedParams = params.split(" ")
-		.map(param => `\"${param}\"`)
+	const modifiedParams = params
+		.replace(":", " ") // handle searches in format of "Exodus 22:30"
+		.split(" ") // split on words and phrases
+		.map((param) => `\"${param}\"`)
 		.join(" ");
-	console.log(modifiedParams)
+	console.log(modifiedParams);
 
 	return `${modifiedParams}`;
-}
+};
 
 exports.searchData = async (req, res, next) => {
-
 	const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.wcadp.mongodb.net/aquinas?retryWrites=true&w=majority`;
 	const client = new MongoClient(uri, {
 		useNewUrlParser: true,
@@ -30,34 +29,35 @@ exports.searchData = async (req, res, next) => {
 
 	client.connect(async (err) => {
 		try {
-			if(!req.params?.searches?.length){
+			if (!req.params?.searches?.length) {
 				await res.status(500).json({
 					status: "error",
-					data: { 
+					data: {
 						message: "Params are not an array",
-						params: params
+						params: params,
 					},
 				});
 			}
 
-			console.log(req.params)
+			console.log(req.params);
 			const db = await client.db("aquinas");
-			
-			const docs = await db.collection("data")
-				.find({$text: {$search: queryifySearchTerms(req.params.searches)}})
+
+			const docs = await db
+				.collection("data")
+				.find({ $text: { $search: queryifySearchTerms(req.params.searches) } })
 				.toArray();
 
 			await res.status(203).json({
 				status: "success",
-				data: docs
-			})
+				data: docs,
+			});
 			client.close();
 		} catch (err) {
 			await res.status(500).json({
 				status: "error",
 				data: { message: err.message },
 				query: req.query,
-				params: req.params
+				params: req.params,
 			});
 			client.close();
 		}
